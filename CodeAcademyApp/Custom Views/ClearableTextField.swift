e6476542aa23381009c119fce664eb3236efeb00
TonyJem <a.jemanov@gmail.com>
2021-02-07 20:36:36 +0200

    Create new "ClearableTextField"

---
+++ CodeAcademyApp/Custom Views/ClearableTextField.swift
@@ -0,0 +1,20 @@
+//
+//  ClearableTextField.swift
+//  CodeAcademyApp
+//
+//  Created by Office-iMac on 2021-02-07.
+//
+
+import UIKit
+
+class ClearableTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
