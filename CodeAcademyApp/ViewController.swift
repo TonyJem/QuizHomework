35d295e0e3d892d2a06b8573d7fc76a60f61ec75
TonyJem <a.jemanov@gmail.com>
2021-02-07 08:46:11 +0200

    WIP: for Task02 Can't lounch proceedToQuizView() from ViewController

--- CodeAcademyApp/ViewController.swift
+++ CodeAcademyApp/ViewController.swift
@@ -2,19 +2,19 @@ import UIKit
 
 class ViewController: CodeAcademyViewController {
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print("🟢 VC loaded")
         
-        guard let curentUser = UserDefaultsManager.currentUser else { return }
+        guard let curentUser = UserDefaultsManager.currentUser else {
+            print("🟢 VC loaded")
+            return
+        }
         
         print("🟢🟢🟢 curentUser in VC didDetected: \(curentUser)")
         proceedToQuizView()
     }
-    
-    
     
     @IBAction func loginButtonTApped(_ sender: UIButton) {
         
