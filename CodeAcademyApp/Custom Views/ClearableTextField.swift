c50b23c855562577901a914a48795c86c2af9ac7
TonyJem <a.jemanov@gmail.com>
2021-02-07 20:38:54 +0200

    Update ClearableTextField.swift

--- CodeAcademyApp/Custom Views/ClearableTextField.swift
+++ CodeAcademyApp/Custom Views/ClearableTextField.swift
@@ -1,20 +1,61 @@
-//
-//  ClearableTextField.swift
-//  CodeAcademyApp
-//
-//  Created by Office-iMac on 2021-02-07.
-//
-
 import UIKit
 
+typealias VoidCompletion = () -> Void
+
+protocol ClearableTextFieldDelegate: AnyObject {
+    func didTapClearIcon(_ icon: UIImageView)
+}
 class ClearableTextField: UITextField {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+    private let TextInset: CGFloat = 10
+    private let IconFrame = CGRect(x: .zero, y: .zero, width: 20, height: 10)
 
+    weak var clearableTextFieldDelegate: ClearableTextFieldDelegate?
+    var onDidTap: VoidCompletion?
+
+    private lazy var clearIconView: UIImageView = {
+        let clearIcon =  #imageLiteral(resourceName: "clear-icon").withTintColor(.placeholderText)
+        let imageView = UIImageView(image: clearIcon)
+
+        let tapGestureRecognizer = UITapGestureRecognizer(
+            target: self,
+            action: #selector(clearIconWasTapped)
+        )
+        imageView.isUserInteractionEnabled = true
+        imageView.addGestureRecognizer(tapGestureRecognizer)
+
+        return imageView
+    }()
+
+    override func didMoveToSuperview() {
+        super.didMoveToSuperview()
+        configureView()
+    }
+}
+
+// MARK: - Helpers
+extension ClearableTextField {
+
+    private func configureView() {
+        rightViewMode = .whileEditing
+        rightView = clearIconView
+        backgroundColor = .white
+    }
+
+    @objc private func clearIconWasTapped() {
+        text = nil
+        clearableTextFieldDelegate?.didTapClearIcon(clearIconView)
+        onDidTap?()
+    }
+
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        let insets = UIEdgeInsets(top: .zero, left: TextInset, bottom: .zero, right: .zero)
+        return bounds.inset(by: insets)
+    }
+
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        let insets = UIEdgeInsets(top: .zero, left: TextInset, bottom: .zero, right: .zero)
+        return bounds.inset(by: insets)
+    }
+    
 }
